b3e9ae0982ae301ec1164403b99e42ede6b15a92
kamiaki <247557768@qq.com>
2020-08-29 18:39:43 +0800

    模板模式

---
+++ src/main/java/com/aki/designpattern/模板模式/Football.java
@@ -0,0 +1,19 @@
+package com.aki.designpattern.模板模式;
+
+public class Football extends Game {
+
+   @Override
+   void endPlay() {
+      System.out.println("Football Game Finished!");
+   }
+
+   @Override
+   void initialize() {
+      System.out.println("Football Game Initialized! Start playing.");
+   }
+
+   @Override
+   void startPlay() {
+      System.out.println("Football Game Started. Enjoy the game!");
+   }
+}
---
+++ src/main/java/com/aki/designpattern/模板模式/Game.java
@@ -0,0 +1,20 @@
+package com.aki.designpattern.模板模式;
+
+public abstract class Game {
+   abstract void initialize();
+   abstract void startPlay();
+   abstract void endPlay();
+
+   //template method
+   public final void play(){
+
+      //initialize the game
+      initialize();
+
+      //start game
+      startPlay();
+
+      //end game
+      endPlay();
+   }
+}
