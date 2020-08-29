c65e0609d48ca4a3054b2aea9a1c18557223339e
kamiaki <247557768@qq.com>
2020-08-29 18:33:02 +0800

    外观模式

---
+++ src/main/java/com/aki/designpattern/外观模式/SubSystemFour.java
@@ -0,0 +1,7 @@
+package com.aki.designpattern.外观模式;
+
+class SubSystemFour{
+    public void methodFour(){
+        System.out.println("子系统方法四");
+    }
+}
---
+++ src/main/java/com/aki/designpattern/外观模式/SubSystemThree.java
@@ -0,0 +1,7 @@
+package com.aki.designpattern.外观模式;
+
+class SubSystemThree{
+    public void methodThree(){
+        System.out.println("子系统方法三");
+    }
+}
