b472d7e5efcf932eb39b095181ac3dbdb2aac9c6
kamiaki <247557768@qq.com>
2020-08-30 14:22:07 +0800

    中介者模式

---
+++ src/main/java/com/aki/designpattern/中介者模式/AbstractMediator.java
@@ -0,0 +1,5 @@
+package com.aki.designpattern.中介者模式;
+
+public abstract class AbstractMediator {
+    public abstract void operate(AbstractColleague colleague, String message);
+}
--- src/main/java/com/aki/designpattern/中介者模式_/TestMain.java
+++
@@ -1,7 +0,0 @@
-package com.aki.designpattern.中介者模式_;
-
-
-public class TestMain {
-    public static void main(String[] args) {
-    }
-}
