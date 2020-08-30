316eb1026d722cd84e7a0de4fe2d3c5b41fee617
kamiaki <247557768@qq.com>
2020-08-30 14:46:07 +0800

    责任链模式

---
+++ src/main/java/com/aki/designpattern/责任链模式/Action.java
@@ -0,0 +1,4 @@
+package com.aki.designpattern.责任链模式;
+    public interface Action {
+      void todo(String type);
+  }
--- src/main/java/com/aki/designpattern/责任链模式_/TestMain.java
+++
@@ -1,7 +0,0 @@
-package com.aki.designpattern.责任链模式_;
-
-
-public class TestMain {
-    public static void main(String[] args) {
-    }
-}
