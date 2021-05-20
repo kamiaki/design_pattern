bac95a0717a00c0cb58b259c830ffb058c151790
kamiaki <247557768@qq.com>
2021-05-20 11:20:44 +0800

    123

--- src/main/java/com/aki/designpattern/行为型/观察者模式/Observerable.java
+++ src/main/java/com/aki/designpattern/行为型/观察者模式/Observerable.java
@@ -1,7 +1,9 @@
 package com.aki.designpattern.行为型.观察者模式;
 
 public interface Observerable {
-    public void registerObserver(Observer o);
-    public void removeObserver(Observer o);
-    public void notifyObservers();
+    void registerObserver(Observer o);
+
+    void removeObserver(Observer o);
+
+    void notifyObservers();
 }
