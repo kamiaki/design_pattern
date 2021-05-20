2d99c16011ce8d4439d61557a16aa70ce114b37b
kamiaki <247557768@qq.com>
2021-05-20 17:28:01 +0800

    123

--- src/main/java/com/aki/designpattern/结构型/代理模式/动态代理/TestMain.java
+++ src/main/java/com/aki/designpattern/结构型/代理模式/动态代理/TestMain.java
@@ -5,7 +5,7 @@ public class TestMain {
     public static void main(String[] args) {
         Service service = new UserServiceImpl();
         MyInvocatioHandler handler = new MyInvocatioHandler(service);
-        Service serviceProxy = (Service)handler.getProxy();
-        serviceProxy.add();
+        service = (Service)handler.getProxy();
+        service.add();
     }
 }
